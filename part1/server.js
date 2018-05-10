918122f96463085fd211739fe2f7dc5136fc6c3e
alphaborel <aaborel@gmail.com>
2018-05-10 13:32:53 -0700

    75 percent Done

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get("/", function(req, res) {
+  let words = fs.readFileSync('index.html', 'utf8');
 });
+
+//### Challenge 1:
+app.get("/hello", function(req, res) {
+  res.send("Hello!");
+});
+
+//### Challenge 4:
+app.get("/verify/:age", function(req, res) {
+  if (`${req.params.age}` > 13) {
+    res.sendStatus(200);
+  } else if (`${req.params.age}` <= 13) {
+    res.sendStatus(403);
+  }
+});
+
+//### Challenge 2:
+app.post("/create/:name", function(req, res) {
+  var output = {
+    "id": 1,
+    "name": `${req.params.name}`
+  }
+  res.json(output);
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+//### Challenge 2:
+app.get("/", function(req, res) {
+  var storage = fs.readFileSync("./storage.json", "UTF-8")
+  res.send(storage);
+});
+
+//### Challenge 1:
+app.post("/create/:name/:age", function(req, res) {
+  var usrInput = {
+    "name": `${req.params.name}`,
+    "age": `${req.params.age}`
+  }
+  var writeable = JSON.stringify(usrInput);
+
+  fs.appendFile('storage.json', writeable, (err) => {
+    if (err) throw err;
+    res.send('The "data to append" was appended to file!');
+  });
 });
 
 app.use(function(req, res) {
